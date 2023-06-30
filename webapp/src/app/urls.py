e5e9a4e71ff55561b329e2c2ec1b7a4002d41044
voidCaloneian <106653303+voidCaloneian@users.noreply.github.com>
2023-06-30 09:18:04 +0500

    Update urls.py

--- webapp/src/app/urls.py
+++ webapp/src/app/urls.py
@@ -5,7 +5,7 @@ from django.urls import include, path
 from .views import (
     CargoFilterAPIView,
     CargoNearestCarsAPIView,
-    CarPatchViewSet,
+    CarPatchAPIView,
     CargoViewSet,
 )
 
@@ -14,7 +14,7 @@ router = DefaultRouter()
 router.register('cargo', CargoViewSet, basename='cargo')
 
 car_urls = [
-    path('patch/<int:id>/', CarPatchViewSet.as_view({'patch': 'partial_update'}), name='car-patch'),
+    path('patch/<int:id>/', CarPatchAPIView.as_view(), name='car-patch'),
 ]
 
 cargo_urls = [
